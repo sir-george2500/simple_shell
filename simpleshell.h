2618371f6a93f990f2fd02f76a68cb9d37f31b68
Deebee-e <elideb4life@gmail.com>
2023-09-17 03:12:07 +0100

    Header file and main file Created

---
+++ simpleshell.h
@@ -0,0 +1,16 @@
+#ifndef SIMPLESHELL_H
+#define SIMPLESHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <signal.h>
+#include <dirent.h>
+#include <stdbool.h>
+
+#endif
