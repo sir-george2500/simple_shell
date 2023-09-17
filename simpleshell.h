43415aaa12a87c2d8bd3705ab8373953838c17a1
George S. Mulbah II <gsmulbah2500@gmail.com>
2023-09-17 06:48:24 +0000

    added the marcos file to simpleshell.h

--- simpleshell.h
+++ simpleshell.h
@@ -1,16 +1,22 @@
 #ifndef SIMPLESHELL_H
 #define SIMPLESHELL_H
+#ifndef SHELL_H
+#define SHELL_H
 
-#include <stdio.h>
+#include <stdio.h> /* for printf*/
+#include <unistd.h> /* for fork, execve*/
 #include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <fcntl.h>
-#include <errno.h>
-#include <signal.h>
-#include <dirent.h>
-#include <stdbool.h>
+#include <string.h> /* for strtok*/
+#include <stddef.h>
+#include <errno.h> /* for errno and perror */
+#include <sys/types.h> /* for type pid */
+#include <sys/wait.h> /* for wait */
+#include <sys/stat.h> /* for use of stat function */
+#include <signal.h> /* for signal management */
+#include <fcntl.h> /* for open files*/
+
+/************* Needed MACROS **************/
+
+#include "macros.h" /* for msg help and prompt */
 
 #endif
